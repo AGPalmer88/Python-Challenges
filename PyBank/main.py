27828be5dc54cdd689b3c8911c1c802db9c4c2df
AGPalmer88 <61656034+AGPalmer88@users.noreply.github.com>
2020-03-23 17:47:53 -0400

    testing commit
    Started the homework for Python but realized the file wouldn't open for Budget_data.csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,12 @@
-main.py
+#main.py - Financial Analysis
+import os
+import csv
+
+#ex: file = '../Resources/input.txt' - "use info from read_csv.py activity 8 in 3-2-activity 8 example"
+
+budget_data = os.path.join('..', 'Resources', 'budget_data.csv')
+#Open and Read Budget data file csv first
+with open(budget_data, 'r') as csvfile:
+
+#The total number of months included in the dataset
+#Total_months = len(months)
